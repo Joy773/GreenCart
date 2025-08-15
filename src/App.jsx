f56be4d6084d6a9d1e95e91dbcd1401df991f568
Joy773 <dibbojitdasjoy@gmail.com>
2025-08-16 02:57:46 +0600

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import {Toaster} from "react-hot-toast"
+import Footer from './components/Footer'
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -16,9 +17,10 @@ const App = () => {
 
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
+          <Route path='/' element={<Home/>} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   )
 }
